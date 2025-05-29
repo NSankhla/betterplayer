0b9e27c12f16dd5af5bae8e9fbde366540e2846c
Nirmal Sankhla <sankhla675291@gmail.com>
2025-05-29 09:06:35 +0530

    upgrade

--- lib/src/hls/hls_parser/hls_track_metadata_entry.dart
+++ lib/src/hls/hls_parser/hls_track_metadata_entry.dart
@@ -14,19 +14,19 @@ class HlsTrackMetadataEntry {
   final String? name;
 
   /// The EXT-X-STREAM-INF tags attributes associated with this track. This field is non-applicable (and therefore empty) if this track is derived from an EXT-X-MEDIA tag.
-  final List<VariantInfo>? variantInfos;
+  final List? variantInfos;
 
   @override
   bool operator ==(dynamic other) {
     if (other is HlsTrackMetadataEntry) {
       return other.groupId == groupId &&
           other.name == name &&
-          const ListEquality<VariantInfo>()
+          const ListEquality()
               .equals(other.variantInfos, variantInfos);
     }
     return false;
   }
 
   @override
-  int get hashCode => hashValues(groupId, name, variantInfos);
+  int get hashCode => Object.hash(groupId, name, variantInfos);
 }
